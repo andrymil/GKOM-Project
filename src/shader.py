1173b6852a30b711b7d3e1ffd4c86e2b0b17c07e
mandrysz <01187204@pw.edu.pl>
2026-05-02 23:43:07 +0200

    feat: add shader handler

---
+++ src/shader.py
@@ -0,0 +1,24 @@
+from OpenGL.GL import *
+import OpenGL.GL.shaders as shaders
+
+
+class ShaderProgram:
+    def __init__(self, vertex_path, fragment_path):
+        self.id = self._compile_from_files(vertex_path, fragment_path)
+
+    def _compile_from_files(self, v_path, f_path):
+        with open(v_path, "r") as f:
+            vertex_code = f.read()
+        with open(f_path, "r") as f:
+            fragment_code = f.read()
+
+        vertex = shaders.compileShader(vertex_code, GL_VERTEX_SHADER)
+        fragment = shaders.compileShader(fragment_code, GL_FRAGMENT_SHADER)
+        return shaders.compileProgram(vertex, fragment)
+
+    def use(self):
+        glUseProgram(self.id)
+
+    def set_mat4(self, name, matrix):
+        location = glGetUniformLocation(self.id, name)
+        glUniformMatrix4fv(location, 1, GL_FALSE, matrix.to_bytes())
